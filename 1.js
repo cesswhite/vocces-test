3d9e8c36341d132e167b1248637b3ffd4d26cd76
cesswhite <acessloop@gmail.com>
2023-04-28 16:55:36 -0600

    [ADD] - Exercise 1 completed

--- 1.js
+++ 1.js
@@ -3,11 +3,26 @@
 // crea una función findUniq que retorne el elemento único.
 
 function findUniq(array) {
+    const freq = {};
+    array.forEach(element => {
+        const _element = element
+        if (freq[_element]) {
+            freq[_element]++
+        } else {
+            freq[_element] = 1
+        }
+    });
+
+    for (const element in freq) {
+        if (freq[element] === 1) {
+            return element;
+        }
+    }
 }
 
 
 /**
  * TEST Ejercicio 1
  */
-findUniq(['12', 10, '12', 11, 1, 11, 10, '12']) // 1
-findUniq(['Capitán América', 'Hulk', 'Deadpool', 'Capitán América', 'Hulk', 'Wonder Woman', 'Deadpool', 'Iron Man', 'Iron Man']) // 'Wonder Woman'
+console.log(findUniq(['12', 10, '12', 11, 1, 1, 40, 11, 10, '12']))
+console.log(findUniq(['Capitán América', 'Hulk', 'Deadpool', 'Capitán América', 'Hulk', 'Wonder Woman', 'Deadpool', 'Iron Man', 'Iron Man']))
