16f13aaf87b6325cc3626dd40d3f39f5be413ee5
cesswhite <acessloop@gmail.com>
2023-04-28 17:23:31 -0600

    [ADD] - Exercise 2 completed

--- 2.js
+++ 2.js
@@ -3,10 +3,22 @@
 // crea una función numbersTop para obtener los tres elementos más repetidos ordenados de forma descendente por número de repeticiones.
 
 function numbersTop(array) {
+    const freq = {};
+    array.forEach(element => {
+        const _element = element
+        if (freq[_element]) {
+            freq[_element]++
+        } else {
+            freq[_element] = 1
+        }
+    });
+
+    const array_sorteed = Object.entries(freq).sort((a, b) => b[1] - a[1])
+    return array_sorteed.slice(0, 3).map(element => element[0])
 }
 
 /**
  * TEST Ejercicio 2
  */
-numbersTop([3, 3, 1, 4, 1, 3, 1, 1, 2, 2, 2, 3, 1, 3, 4, 1]) // [ 1, 3, 2 ]
-numbersTop(['a', 3, 2, 'a', 2, 3, 'a', 3, 4, 'a', 'a', 1, 'a', 2, 'a', 3]) // [ 'a', 3, 2 ]
+console.log(numbersTop([3, 3, 1, 4, 1, 3, 1, 1, 2, 2, 2, 3, 1, 3, 4, 1]))
+console.log(numbersTop(['a', 3, 2, 'a', 2, 3, 'a', 3, 4, 'a', 'a', 1, 'a', 2, '2', 'a', 3, '3', '3', '3', '3', '3']))
