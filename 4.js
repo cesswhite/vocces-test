39eff83f1af9268b5ca7756f40e29df5ad82d24e
cesswhite <acessloop@gmail.com>
2023-04-28 21:06:41 -0600

    [ADD] - Exercise 4 completed

--- 4.js
+++ 4.js
@@ -3,10 +3,10 @@
 // Por algún motivo este código no funciona como se espera y está imprimiendo en consola dos veces 'Adiós'.
 // Podriamos corregir esto de alguna manera modificando sólo el código entre las líneas 15 y 19
 
-const showText = function() {
+const showText = function () {
   return {
     text: 'Adiós',
-    init: function() {
+    init: function () {
       return console.log(this.text);
     }
   }
@@ -14,7 +14,7 @@ const showText = function() {
 
 // --------------- Solo puedes tocar desde aquí ------------- //
 showText.text = 'Hola';
-showText2 = showText;
+showText2 = { ...showText }
 showText2.text = 'Adiós';
 // --------------- hasta aquí ------------------------------- //
 
