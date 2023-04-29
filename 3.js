f416a626fbf3f9bdb57282ae32a7601515ddd3ae
cesswhite <acessloop@gmail.com>
2023-04-28 20:55:51 -0600

    [ADD] - Exercise 3 completed

--- 3.js
+++ 3.js
@@ -4,10 +4,15 @@
 // Ejemplo: en el caso de [1, 5, 3, 5] existen 2 incremetos ( el día 2 respecto al dia 1 y el dia 4 respecto al día 3 )
 
 function increasedMeasurements(measurements) {
+    let counter = 0
+    for (const i in measurements) {
+        if (measurements[i] > measurements[i - 1]) counter++
+    }
+    return counter
 }
 
 /**
- * TEST Ejercicio 1
+ * TEST Ejercicio 3
  */
 const measurements = [245, 248, 259, 190, 180, 185, 191, 185, 188, 189, 204, 213, 215, 227, 222, 221, 236, 235, 236, 232, 224, 221, 228, 234, 226, 227, 228, 230, 232, 234]
-increasedMeasurements(measurements) // 19
+console.log(increasedMeasurements(measurements)) // 19
